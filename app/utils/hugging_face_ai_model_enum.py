5e4916faeae493fde4d5ec0a155462250a523d49
aditya <you@example.com>
2026-02-02 17:37:42 +0530

    Added a new model Deepseek R1 support in the platform

--- app/utils/hugging_face_ai_model_enum.py
+++ app/utils/hugging_face_ai_model_enum.py
@@ -3,5 +3,5 @@ from enum import Enum
 class HuggingFaceModelList(Enum):
     model_list = [
         "meta-llama/Llama-3.1-8B-Instruct",
-        "meta-llama/Llama-3.1-8B-Instruct"
+        "deepseek-ai/DeepSeek-R1"
     ]
